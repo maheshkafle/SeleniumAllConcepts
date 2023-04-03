17f37c705603777576ea3575d4cc32d3a87cc739
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-04 01:00:35 +0400

    Added Thread concepts, creating xpath and cssSelector locators for webElements whose value keeps on changing dynamically

--- src/Locators.java
+++ src/Locators.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Locators {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\OneDrive - Lisec Holding GmbH\\Documents\\Projects\\Udemy\\Introduction\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -20,6 +20,8 @@ public class Locators {
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		//To avoid Nosuchelementexception we need to add some wait in our code which is done in line no. 11.
 		driver.findElement(By.linkText("Forgot your password?")).click();
+		//If there is a transition between one state to other in webpage, then some elements may not load completely, in such case we use Thread.sleep
+		Thread.sleep(1000);
 		//customized Xpath -  //Tagname[@attribute='value']
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
 		//If we don't have unique selectors then how to handle using xpath, using index Hint: check using selectorhub in browser
@@ -27,5 +29,19 @@ public class Locators {
 		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
 		//If we don't have unique selectors then how to handle using css-selector, using :nth-child Hint: check using selectorhub in browser
 		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("John@gmail.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("981234512");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.cssSelector(".infoMsg")).getText());
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		//When the attribute value is dynamic which keeps changing everytime then we use regex in locators too
+		//Example showing to create cssSelector locator using regex
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		//Example showing to create xpath locator using regex
+		driver.findElement(By.xpath("//button[contains(@class, 'submit')]")).click();
+		
+		
 	}
 }
