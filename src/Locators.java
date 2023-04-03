2c9f047720b8bc94ae98afd33d6c10b76c6a66c0
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-03 23:58:53 +0400

    Added css-selector and xpath locators technique when we don't have unique or no attributes at all

--- src/Locators.java
+++ src/Locators.java
@@ -16,11 +16,16 @@ public class Locators {
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
 		driver.findElement(By.className("signInBtn")).click();
-		//Css Selector - Tagname.className, Tagname#Id, If class and Id not present then, Tagname[attribute='value']
+		//Css Selector - Tagname.className, Tagname#Id, If class and Id not present also called customized cssselector then , Tagname[attribute='value'] 
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
 		//To avoid Nosuchelementexception we need to add some wait in our code which is done in line no. 11.
 		driver.findElement(By.linkText("Forgot your password?")).click();
+		//customized Xpath -  //Tagname[@attribute='value']
 		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+		//If we don't have unique selectors then how to handle using xpath, using index Hint: check using selectorhub in browser
+		driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("john@e.com");
+		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+		//If we don't have unique selectors then how to handle using css-selector, using :nth-child Hint: check using selectorhub in browser
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("John@gmail.com");
 	}
-
 }
