613db3de0ac580f9275745e10f80714a20d0ee17
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-07 23:19:12 +0400

    Added automating browser navigation and window properties concepts

---
+++ src/WindowActivities.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\OneDrive - Lisec Holding GmbH\\Documents\\Projects\\Udemy\\Introduction\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		driver.get("https://rahulshettyacademy.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+	}
+
+}
