82c3d0654a7a9fe42d96ee3b987ae9876c1e32f9
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-08 01:40:12 +0400

    Added automating clicking and increasing count of dropdown value and updating text with increased count value

---
+++ src/UpdatedDropDown.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//Chrome Browser
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\OneDrive - Lisec Holding GmbH\\Documents\\Projects\\Udemy\\Introduction\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000);
+//		driver.findElement(By.id("hrefIncAdt")).click();
+		int i=1;
+		while(i<5) {
+			driver.findElement(By.id("hrefIncAdt")).click();
+			i++;
+		}
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());		
+	}
+}
