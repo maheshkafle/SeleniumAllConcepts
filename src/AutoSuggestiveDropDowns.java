d9a13feb1774e9611b04306084aed224dada4852
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-08 22:59:07 +0400

    Added automating auto suggesting dropdowns and clicking option concept

---
+++ src/AutoSuggestiveDropDowns.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AutoSuggestiveDropDowns {
+
+	public static void main(String[] args) throws InterruptedException {
+		//Chrome Browser
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\OneDrive - Lisec Holding GmbH\\Documents\\Projects\\Udemy\\Introduction\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("autosuggest")).sendKeys("nor");
+		Thread.sleep(2000);
+		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		for(WebElement option : options) {
+			if (option.getText().equalsIgnoreCase("Norway")) {
+				System.out.println(option.getText());
+				option.click();
+				break;
+			}
+		}
+		
+
+	}
+
+}
