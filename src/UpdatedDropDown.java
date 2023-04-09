1f7464b35c935b4e6c677e3c1b8e0b455e4d921d
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-09 18:56:10 +0400

    Added Assert.assert methods inside Tests

--- src/UpdatedDropDown.java
+++ src/UpdatedDropDown.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 
 public class UpdatedDropDown {
 
@@ -10,8 +12,10 @@ public class UpdatedDropDown {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\OneDrive - Lisec Holding GmbH\\Documents\\Projects\\Udemy\\Introduction\\Drivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		
+		driver.manage().window().maximize();
 		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		//Hint: Incase of Assert.assert stmt. if test fails, then only program execution fails else nothing happens
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "1 Adult");
 		driver.findElement(By.id("divpaxinfo")).click();
 		Thread.sleep(2000);
 //		driver.findElement(By.id("hrefIncAdt")).click();
@@ -21,6 +25,8 @@ public class UpdatedDropDown {
 			i++;
 		}
 		driver.findElement(By.id("btnclosepaxoption")).click();
-		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());		
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		//Hint: Incase of Assert.assert stmt. if test fails, then only program execution fails else nothing happens
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 	}
 }
