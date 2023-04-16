daa84f383b7e1099600d1f3f88e1439e72684cfb
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-16 19:03:33 +0400

    Added child, sub-child window-handle concepts

--- src/MouseOver.java
+++ src/MouseOver.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -15,5 +16,6 @@ public class MouseOver {
 		driver.get("https://www.amazon.com/");
 		Actions a = new Actions(driver);
 		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
 	}
 }
