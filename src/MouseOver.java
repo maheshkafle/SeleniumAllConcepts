2329c0e689d67d7e4b661dd4a5e7d27ba67dbdfe
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-16 15:20:35 +0400

    Added mouseOver action using Actions class in Selenium

---
+++ src/MouseOver.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class MouseOver {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kaflemah\\OneDrive - Lisec Holding GmbH\\Documents\\Projects\\Udemy\\Introduction\\Drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+	}
+}
