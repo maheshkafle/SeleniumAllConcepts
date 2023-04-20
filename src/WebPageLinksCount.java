cb6bf6f21e049760e8321ce22e3ec31f0224128c
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-21 00:05:55 +0400

    Opened all links in new tab using keys.chord method

--- src/WebPageLinksCount.java
+++ src/WebPageLinksCount.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,7 +27,8 @@ public class WebPageLinksCount {
 		
 		//3. click on each link in column and check if pages are opening
 		for(int i=1; i< totalLinks; i++) {
-			columndriver.findElements(By.tagName("a")).get(i).click();
+			String clickOnLinkToOpenInTab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkToOpenInTab);
 		}
 		
 	}
