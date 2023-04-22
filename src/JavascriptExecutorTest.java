2744759e8f3114acb42e0d5c19f0e60eac8aeb28
MaheshKafle <mahesh.kafle@lisec.com>
2023-04-23 02:09:41 +0400

    Handle Table data, parse texts to add all the data in it and add assertions

--- src/JavascriptExecutorTest.java
+++ src/JavascriptExecutorTest.java
@@ -1,5 +1,11 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 
 public class JavascriptExecutorTest {
@@ -13,5 +19,15 @@ public class JavascriptExecutorTest {
 		js.executeScript("window.scrollBy(0, 500)");	
 		Thread.sleep(3000);
 	    js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+	    
+	    List<WebElement> data = driver.findElements(By.cssSelector("div[class='tableFixHead'] td:nth-child(4)"));
+	    int actualSum=0;
+	    for(int i=0; i<data.size(); i++) {
+	    	String parseText = driver.findElements(By.cssSelector("div[class='tableFixHead'] td:nth-child(4)")).get(i).getText();
+	    	actualSum = actualSum + Integer.parseInt(parseText);
+	    }
+	    System.out.println(actualSum);
+	    int expectedValue = Integer.parseInt(driver.findElement(By.className("totalAmount")).getText().split(":")[1].trim());
+	    Assert.assertEquals(actualSum, expectedValue);
 	}
 }
